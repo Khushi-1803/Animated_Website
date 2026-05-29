1008abbb24211e109fe7e4efcdc6bd15493e105b
Khushi-1803 <khushi011504@gmail.com>
2026-05-29 18:23:19 +0530

    final

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -45,21 +45,18 @@ const Agence = () => {
   useGSAP(() => {
   gsap.to(imageDivRef.current, {
     scrollTrigger: {
-      trigger: imageDivRef.current,
-      start: "top 29%",
-      end: "top -100%",
-      scrub: true,
-      pin: true,
-
-      onUpdate: (elem) => {
-        let imageIndex = Math.min(
-          imageArray.length - 1,
-          Math.floor(elem.progress * imageArray.length)
-        );
-
-        imageRef.current.src = imageArray[imageIndex];
-      },
-    },
+  trigger: imageDivRef.current,
+  markers: true,
+  start: 'top 28%',
+  end: 'top -70%',
+  pin: true,
+  pinSpacing: true,
+  pinReparent: true,
+  pinType: 'transform',
+  scrub: 1, // smooth scrubbing with 1s easing
+  anticipatePin: 1,
+  invalidateOnRefresh: true,
+},
   });
 }, []);
   
